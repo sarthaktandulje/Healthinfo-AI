02b91062bfb7f33f854ac2b76bae3f21c5a17007
sarthaktandulje <sarthak.tandulje.portfolio@gmail.com>
2025-11-20 20:48:59 +0530

    Fix model download links

--- download_models.sh
+++ download_models.sh
@@ -4,7 +4,7 @@ echo "⬇️ Downloading REAL MODEL FILES..."
 
 mkdir -p model
 
-curl -L -o model/xray_model.keras "https://drive.google.com/uc?export=download&id=YOUR_XRAY_FILE_ID"
-curl -L -o model/skin_model_final.keras "https://drive.google.com/uc?export=download&id=YOUR_SKIN_FILE_ID"
+curl -L -o model/xray_model.keras "https://drive.google.com/uc?export=download&id=1wPLKWgWPA5sXBaeKSXJrugnfH-ZVCCfb"
+curl -L -o model/skin_model_final.keras "https://drive.google.com/uc?export=download&id=1ew3WsO2vYLZypI_rhGuvwRRIWMH9t5ug"
 
 echo "✅ Models downloaded successfully!"
