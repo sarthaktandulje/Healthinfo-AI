20100fd0487c752eebda15fcd9cdb7da2a6283ef
sarthaktandulje <sarthak.tandulje.portfolio@gmail.com>
2025-11-21 13:57:17 +0530

    Fix Google Drive direct download links

--- download_models.sh
+++ download_models.sh
@@ -4,7 +4,10 @@ echo "⬇️ Downloading REAL MODEL FILES..."
 
 mkdir -p model
 
+# X-ray model
 curl -L -o model/xray_model.keras "https://drive.google.com/uc?export=download&id=1wPLKWgWPA5sXBaeKSXJrugnfH-ZVCCfb"
+
+# Skin model
 curl -L -o model/skin_model_final.keras "https://drive.google.com/uc?export=download&id=1ew3WsO2vYLZypI_rhGuvwRRIWMH9t5ug"
 
 echo "✅ Models downloaded successfully!"
