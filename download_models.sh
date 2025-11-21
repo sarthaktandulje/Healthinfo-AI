73d054a11cbe46e1588b970e9b8d38123727c2c4
sarthaktandulje <sarthak.tandulje.portfolio@gmail.com>
2025-11-21 15:06:46 +0530

    Fix: replaced wget with curl for Railway

--- download_models.sh
+++ download_models.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-
 set -e
 
 echo "📥 Creating model directory..."
 mkdir -p model
 
 echo "⬇️ Downloading X-Ray model..."
-wget -O model/xray_model.keras "https://drive.google.com/uc?export=download&id=1wPLKWgWPA5sXBaeKSXJrugnfH-ZVCCfb"
+curl -L "https://drive.google.com/uc?export=download&id=1wPLKWgWPA5sXBaeKSXJrugnfH-ZVCCfb" -o model/xray_model.keras
 
 echo "⬇️ Downloading Skin model..."
-wget -O model/skin_model_final.keras "https://drive.google.com/uc?export=download&id=1ew3WsO2vYLZypI_rhGuvwRRIWMH9t5ug"
+curl -L "https://drive.google.com/uc?export=download&id=1ew3WsO2vYLZypI_rhGuvwRRIWMH9t5ug" -o model/skin_model_final.keras
 
 echo "✅ All models downloaded successfully!"
